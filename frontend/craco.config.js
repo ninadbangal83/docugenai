6aaec3430ac52d5df40dab67f9d2fa7cb2b73c6f
ninadbangal83 <bangalniand83@gmail.com>
2025-07-07 14:45:18 +0530

    - Installed necessary dependencies - Added typescript, eslint and jest configuration files - Added docker files and created docker compose file for both services

---
+++ frontend/craco.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+/** @type {import('@craco/craco').CracoConfig} */
+module.exports = {
+  webpack: {
+    alias: {
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@services': path.resolve(__dirname, 'src/services'),
+      '@store': path.resolve(__dirname, 'src/store'),
+      '@hooks': path.resolve(__dirname, 'src/services/hooks'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@types': path.resolve(__dirname, 'src/types'),
+    }
+  }
+};
